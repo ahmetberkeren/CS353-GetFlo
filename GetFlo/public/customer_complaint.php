ace01ac74fe301e06c1ae88c219017f9780e0cef
mehmetakifkilic <47590933+mehmetakifkilic@users.noreply.github.com>
2020-05-22 01:06:18 +0300

    Add files via upload

--- GetFlo/public/customer_complaint.php
+++ GetFlo/public/customer_complaint.php
@@ -1,4 +1,4 @@
-<?php include "templates/header.php";
+<?php
 require "../config.php";
 require "../common.php";
 session_start(); ?>
@@ -9,29 +9,39 @@ if (isset($_POST['finish'])) {
         $tmpID = $_SESSION['orderID'];
         $subject = $_POST["subject"];
         $message = $_POST["message"];
-
-        $sql = "Insert Into complaint_form(orderID, message, subject) Values(:tmpID ,:message , :subject)";
+        $answer = false;
+        $sql = "Insert Into complaint_form(orderID, message, subject , is_answered ) Values(:tmpID ,:message , :subject , :answer)";
         $statement = $connection->prepare($sql);
         $statement->bindValue(':tmpID', $tmpID);
         $statement->bindValue(':message', $message);
         $statement->bindValue(':subject', $subject);
+        $statement->bindValue(':answer', $answer);
 
         $statement->execute();
     } catch (PDOException $error) {
         echo $sql . "<br>" . $error->getMessage();
     }
 }
-    ?>
-
-<li>
-    <textarea name="subject" rows="5" cols="40" placeholder="Subject.." required>
+?>
+<?php
+$tmpID = $_SESSION['orderID'];
+echo "<h1 align='center' style = 'color: red'> {dfdfs} </h1>";
+?>
+<html>
+<body>
+<li align = "center" >
+    <textarea  name="subject" rows="5" cols="40" placeholder="Subject.." >
     </textarea>
-
-    <textarea name="complaint" rows="40" cols="40" placeholder="Enter your complaint.." required>
-    </textarea>
-
-    <input type = "submit" name = "finish" value = "Finish" >
-
 </li>
+<li align = "center"  >
+    <textarea  name="complaint" rows="10" cols="40" placeholder="Enter your complaint.." >
+    </textarea>
+</li>
+<li align = "center">
+    <input type = "submit" name = "finish" value = "Finish" > </input>
+</li>
+</body>
+</html>
+
 
 <?php include "templates/footer.php"; ?>
